c48ce897809f60e2614fb8d69774c4bfedd41b4d
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-02-16 23:53:16 +0900

    practice var & const

--- HelloWorld/helloworld.go
+++ HelloWorld/helloworld.go
@@ -3,5 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World")
+	var greet string = "Hello, World"
+
+	const greet_morning string = "Good Mornig"
+
+	const (
+		a = 1 + 2
+		b
+		c
+	)
+
+	const (
+		d = iota
+		e
+	)
+
+	const (
+		f = 1 << iota
+		g
+		h
+	)
+
+	fmt.Println(greet)
+	fmt.Println(greet_morning)
+	fmt.Println(a, b, c)
+	fmt.Println(d, e, f, g, h)
 }
